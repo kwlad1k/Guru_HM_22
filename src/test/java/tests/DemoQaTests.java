c191cd2d99b8b46e741a6091d2d79caefea56e00
Kwlad1ck <vlad-kad00@mail.ru>
2024-03-30 11:07:25 +0300

    add Extension

--- src/test/java/tests/DemoQaTests.java
+++ src/test/java/tests/DemoQaTests.java
@@ -1,53 +1,22 @@
 package tests;
 
+import extensions.WithLogin;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
-import models.LoginBodyModel;
-import models.LoginResponseModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Cookie;
 import pages.ProfilePage;
-
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static io.qameta.allure.Allure.step;
-import static io.restassured.RestAssured.given;
-import static specs.LoginSpec.*;
 
 public class DemoQaTests extends TestBase {
 
     ProfilePage profilePage = new ProfilePage();
-    TestData testData = new TestData();
 
     @Test
+    @WithLogin
     @Owner("Kwlad1ck")
-    @DisplayName("Авторизация юзреа")
-    void emptyBookListTest() {
-        LoginBodyModel loginData = new LoginBodyModel();
-        loginData.setUserName(testData.username);
-        loginData.setPassword(testData.password);
-
-        LoginResponseModel loginResponseModel = step("Make request", () ->
-                given(defaultLoggingSpec)
-                        .body(loginData)
-
-                        .when()
-                        .post("/Account/v1/Login")
-
-                        .then()
-                        .spec(loginUserResponseSpec)
-                        .statusCode(200)
-                        .extract().as(LoginResponseModel.class));
-        
-        open("https://demoqa.com/images/Toolsqa.jpg");
-        getWebDriver().manage().addCookie(
-                new Cookie("userID", loginResponseModel.getUserId()));
-        getWebDriver().manage().addCookie(
-                new Cookie("token", loginResponseModel.getToken())
-        );
-        getWebDriver().manage().addCookie(
-                new Cookie("expires", loginResponseModel.getExpires())
-        );
+    @Feature("Profile")
+    @DisplayName("Проверка пустого списка книг")
+    void emptyBookListWithLoginExtensionsTest() {
 
         profilePage.openPage()
                 .checkDataElement();
--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -5,6 +5,6 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class TestData {
     AuthConfig authConfig = ConfigFactory.create(AuthConfig.class);
-    String username = authConfig.userName(),
-    password = authConfig.password();
+    public String username = authConfig.userName();
+    public String password = authConfig.password();
 }
